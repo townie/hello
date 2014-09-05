35e19863e6c32868b3dae54527a1c4bb61c71f65
Keith Webber <kwebber@backupify.com>
2014-09-04 23:27:00 -0400

    weather underground web server

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "net/http"
+
 func main() {
-  println("hello")
+  http.HandleFunc("/", hello)
+  http.ListenAndServe(":8080", nil)
 }
+
+func hello(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("hello!"))
+}
+
+type weatherData struct {
+  Name string 'json:"name"'
+  Main struct {
+    Kelvin float64 'json:"temp"'
+  } 'json:"main"'
+}
+
+func
